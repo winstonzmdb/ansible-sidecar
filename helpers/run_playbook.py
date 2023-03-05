7d991ee9422be7d3506839c7567a239540b9e625
winstonzmdb <winston.zhao@mongodb.com>
2023-03-04 22:51:55 -0800

    Added console log for playbook cli command

--- helpers/run_playbook.py
+++ helpers/run_playbook.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 import logging
 
@@ -6,4 +5,5 @@ def run_playbook(file_path):
     # Run the new playbook
     logging.info(f"Running updated tasks")
     command = f"ansible-playbook {file_path}"
+    logging.info(command)
     subprocess.call(command, shell=True)
